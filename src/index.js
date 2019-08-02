5fa1b35310037528674a167bef911e2dc9cdcb3c
gatmeister <almarissa.elemino@gmail.com>
2019-08-02 14:37:16 -0300

    testing commits via vscode

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import FlexBoxes from './js/FlexDemo.js';
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<FlexBoxes />, document.getElementById('root2'));
 ReactDOM.render(<FlexBoxes />, document.getElementById('root3'));
+ReactDOM.render(<FlexBoxes />, document.getElementById('root4'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
